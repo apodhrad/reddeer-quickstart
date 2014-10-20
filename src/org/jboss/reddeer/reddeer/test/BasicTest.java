c9171907d3ec1514dbe62bd0f67ba89c5a4f3dc4
Andrej Podhradsky <apodhrad@redhat.com>
2014-10-20 10:08:55 +0200

    Updated BasicTest

--- src/org/jboss/reddeer/reddeer/test/BasicTest.java
+++ src/org/jboss/reddeer/reddeer/test/BasicTest.java
@@ -1,18 +1,38 @@
 package org.jboss.reddeer.reddeer.test;
 
-import static org.junit.Assert.assertTrue;
-
 import org.jboss.reddeer.junit.runner.RedDeerSuite;
+import org.jboss.reddeer.swt.condition.ShellWithTextIsAvailable;
+import org.jboss.reddeer.swt.impl.button.PushButton;
+import org.jboss.reddeer.swt.impl.menu.ShellMenu;
 import org.jboss.reddeer.swt.impl.shell.DefaultShell;
+import org.jboss.reddeer.swt.impl.text.LabeledText;
+import org.jboss.reddeer.swt.impl.tree.DefaultTreeItem;
+import org.jboss.reddeer.swt.wait.WaitWhile;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+/**
+ * <h3>RedDeer Quickstart</h3>
+ * 
+ * A basic test which creates a general project. You can run the test as follows:
+ * 
+ * <p>Run > Run Configurations... > RedDeer Test > Basic Test</p>
+ * 
+ * @author Andrej Podhradsky
+ *
+ */
 @RunWith(RedDeerSuite.class)
 public class BasicTest {
 
 	@Test
 	public void basicTest() {
-		assertTrue(new DefaultShell().getText() != null);
+		new ShellMenu("File", "New", "Project...").select();
+		new DefaultShell("New Project");
+		new DefaultTreeItem("General", "Project").select();
+		new PushButton("Next >").click();
+		new LabeledText("Project name:").setText("general");
+		new PushButton("Finish").click();
+		new WaitWhile(new ShellWithTextIsAvailable("New Project"));
 	}
 
 }
